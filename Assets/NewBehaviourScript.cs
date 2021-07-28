d3bec0dd0b3051fa644fa2dd005d15a64ecfa692
Michael Yap <michael.yap@carvana.com>
2021-07-28 15:52:03 -0600

    front and backward script

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -2,8 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class HaulerMovement : MonoBehaviour
 {
+    public RigidBody rb;
+    public Transform car;
+    public float speed = 17;
+    Vector3 forward = new Vector3(0,0,1);
+    Vector3 backward = new Vector3(0,0,-1);
+
+    void FixedUpdate()
+    {
+        if(Input.GetKey("Space")){
+            rb.MovePosition(car.position + forward * speed * Time.deltaTime);
+        }
+        if(Input.GetKey("s")){
+            rb.MovePosition(car.position + backward * speed * Time.deltaTime);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
